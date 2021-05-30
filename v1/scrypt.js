d03d1ee3bf0d1b089eb150ecbda4bef84442e627
MUSHA170 <61752212+MUSHA170@users.noreply.github.com>
2021-05-29 23:22:59 -0300

    v1
    by Airton

---
+++ v1/scrypt.js
@@ -0,0 +1,9 @@
+function alerta(){
+    obj = document.getElementById("nome_evento");
+    var nome_evento = obj.value;
+    alert("nome evento =" + nome_evento);
+}
+
+/* function pagina(){
+    if 
+} */
