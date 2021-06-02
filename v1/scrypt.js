83b3033096021ffa7cdb8587697fe0f6162bac2f
MUSHA170 <61752212+MUSHA170@users.noreply.github.com>
2021-06-02 19:21:39 -0300

    Add funct button|evento nome
    index - botões de escolha funcionais
    e nome do evento guardado

--- v1/scrypt.js
+++ v1/scrypt.js
@@ -1,9 +1,1 @@
-function alerta(){
-    obj = document.getElementById("nome_evento");
-    var nome_evento = obj.value;
-    alert("nome evento =" + nome_evento);
-}
 
-/* function pagina(){
-    if 
-} */
